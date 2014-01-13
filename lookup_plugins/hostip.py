252979002f8e272964104884d4ee4be2969e0a3e
rpedde <ron@pedde.com>
2014-01-13 17:02:37 -0600

    rename ipincidr to hostip
    add ability to lookup the address of a different host
    again, as partial implementation of #13

--- lookup_plugins/hostip.py
+++ lookup_plugins/hostip.py
@@ -1,19 +1,17 @@
-# (c) 2012, Jan-Piet Mens <jpmens(at)gmail.com>
+# Copyright (c) 2014 Ron Pedde <ron@pedde.com>
 #
-# This file is part of Ansible
-#
-# Ansible is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ansible is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ansible import utils, errors
 import os
@@ -24,6 +22,9 @@ class LookupModule(object):
     def __init__(self, basedir=None, **kwargs):
         self.basedir = basedir
 
+    def templatize(self, term, inject=None):
+        return utils.template.template(self.basedir, term, inject)
+
     def run(self, terms, inject=None, **kwargs):
         terms = utils.listify_lookup_plugin_terms(terms, self.basedir, inject)
 
@@ -32,12 +33,27 @@ class LookupModule(object):
 
         ret = []
 
+        hostname = kwargs['vars'].get('inventory_hostname', 'unknown')
+
         for term in terms:
-            net = ipaddr.IPv4Network(term)
+            termlist = term.split()
+            termdict = dict([(item.split('=', 1)[0],
+                              self.templatize(item.split('=', 1)[1], inject))
+                             for item in termlist])
+
+
+            hostvars = kwargs['vars']
+            if 'host' in termdict:
+                hostvars = kwargs['vars']['hostvars'].get(termdict['host'], {})
+
+            # sensible defaults?
+            cidr = termdict.get('cidr', '0.0.0.0/32')
+
+            net = ipaddr.IPv4Network(cidr)
 
             found_addr = None
 
-            for addr in kwargs['vars'].get('ansible_all_ipv4_addresses', []):
+            for addr in hostvars.get('ansible_all_ipv4_addresses', []):
                 if ipaddr.IPv4Address(addr) in net:
                     found_addr = addr
 
@@ -52,7 +68,8 @@ class LookupModule(object):
 
             if found_addr is None:
                 # raise?
-                found_addr = kwargs['vars'].get('ansible_default_ipv4', '0.0.0.0')
+                found_addr = hostvars.get('ansible_default_ipv4', '0.0.0.0')
 
             ret.append(found_addr)
+
         return ret
